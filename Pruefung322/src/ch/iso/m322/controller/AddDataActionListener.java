f54d1d493bb4fa0046266d87b38b90e42258a97a
AlexMarrer <95776537+RayMatador@users.noreply.github.com>
2023-11-24 21:20:20 +0100

    feat(BE): created project #Develop
    No Comments
    refs: #Develope

---
+++ Pruefung322/src/ch/iso/m322/controller/AddDataActionListener.java
@@ -0,0 +1,33 @@
+package ch.iso.m322.controller;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+import ch.iso.m322.model.Eintrag;
+import ch.iso.m322.util.ReferenceFinder;
+import ch.iso.m322.view.MyFrame;
+
+public class AddDataActionListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		final Component item = (Component) e.getSource();
+		final JFrame frame = ReferenceFinder.findFrame(item);
+		
+		Eintrag eintrag = ((MyFrame) frame).getData();
+		
+		
+		final JList<String> list = ((MyFrame) frame).getMyJList();
+		final DefaultListModel model = (DefaultListModel) list.getModel();
+		
+		// hilfe
+		
+	}
+
+}
